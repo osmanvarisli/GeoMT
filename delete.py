5f4d79024ebe41e1552b5fb6f8f81f8f726c9ee0
Osman VARIŞLI <64133166+osmanvarisli@users.noreply.github.com>
2022-09-24 22:35:51 +0300

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,21 +1,21 @@
+"""
+@author: Osman VARIŞLI
+"""
 import os 
 import datetime
 import glob
 path = r'C:\Users\PC\Desktop\Flask\UsersFiles'
 
-today = datetime.datetime.today()#gets current time
-os.chdir(path) #changing path to current path(same as cd command)
+today = datetime.datetime.today()
+os.chdir(path) 
 
-#we are taking current folder, directory and files 
-#separetly using os.walk function
+
 for root,directories,files in os.walk(path,topdown=False): 
     for name in files:
-        #this is the last modified time
+
         t = os.stat(os.path.join(root, name))[8] 
         filetime = datetime.datetime.fromtimestamp(t) - today
 
-        #checking if file is more than 7 days old 
-        #or not if yes then remove them
         if filetime.days <= -7:
             print(os.path.join(root, name), filetime.days)
             os.remove(os.path.join(root, name))
